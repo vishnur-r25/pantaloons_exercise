58921857d6fc08bb754987e1734855a6ecf440b4
vishnur-r25 <vishnu.r@codingmart.com>
2026-01-08 12:32:54 +0530

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "@/styles/header.css"
+
+function Header() {
+  return (
+    <div className="header-container">
+        
+        header
+    </div>
+  )
+}
+
+export default Header
